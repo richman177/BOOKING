95e2903328606c9660b309bd6174953293d54666
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-06-10 09:23:12 +0600

    Update settings.py
    dgrdg

--- booking_site/booking_site/settings.py
+++ booking_site/booking_site/settings.py
@@ -144,4 +144,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'booking.UserProfile'
+AUTH_USER_MODEL = 'booking.UserProfile'     
