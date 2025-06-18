af34263fc2eaef76a2a7da6988a058de9cac5eac
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-06-18 11:28:31 +0600

    Update settings.py
    cxv

--- booking_site/booking_site/settings.py
+++ booking_site/booking_site/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-]
+]   
 
 ROOT_URLCONF = 'booking_site.urls'
 
