6f33781902d8b16deb07401f6c8359ed5117d2bf
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-07-01 09:28:29 +0600

    Update models.py
    w

--- booking_site/booking/models.py
+++ booking_site/booking/models.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from django.db import models 
 from django.contrib.auth.models import AbstractUser 
 from phonenumber_field.modelfields import PhoneNumberField 
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator 
 
 
 
