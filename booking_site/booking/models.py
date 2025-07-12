2f58163f1df6bf47e9694be0d64a52522b56b6ea
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-07-12 10:59:26 +0600

    Update models.py
    b

--- booking_site/booking/models.py
+++ booking_site/booking/models.py
@@ -22,7 +22,7 @@ class UserProfile(AbstractUser):
 
 
 class Hotel(models.Model):
-    hotel_name = models.CharField(max_length=60)
+    hotel_name = models.CharField(max_length=60) 
     country = models.CharField(max_length=30)
     city = models.CharField(max_length=30)
     address = models.CharField(max_length=30)
